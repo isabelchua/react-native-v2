597a88d1abf04474e7b55209f2efd8efddde3cbc
isabelchua <isabelchua86@gmail.com>
2021-02-03 21:53:18 +0800

    initial commit

---
+++ TeslaClone/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.carContainer}>
+				<View style={styles.titles}>
+					<Text style={styles.title}>Model S</Text>
+					<Text style={styles.subtitle}>Starting at $69,420</Text>
+				</View>
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#009ad6",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+
+	carContainer: {
+		width: "100%",
+		height: "100%"
+	},
+
+	titles: {
+		marginTop: "30%",
+		width: "100%",
+		alignItems: "center"
+	},
+	title: {
+		fontSize: 40,
+		fontWeight: "600"
+	},
+	subtitle: {}
+});
