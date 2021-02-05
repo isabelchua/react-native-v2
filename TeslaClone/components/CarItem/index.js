8ebaade08bac428a5db533c93d871d8eb43ec654
isabelchua <isabelchua86@gmail.com>
2021-02-05 10:52:15 +0800

    linked component

--- TeslaClone/components/CarItem/index.js
+++ TeslaClone/components/CarItem/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
+import styles from "./styles";
 
 const CarItem = props => {
 	return (
-		<View>
-			<Text>CarItem</Text>
+		<View style={styles.carContainer}>
+			<ImageBackground
+				source={require("../../assets/images/ModelX.jpeg")}
+				style={styles.image}
+			/>
+			<View style={styles.titles}>
+				<Text style={styles.title}>Model S</Text>
+				<Text style={styles.subtitle}>Starting at $69,999</Text>
+			</View>
 		</View>
 	);
 };
