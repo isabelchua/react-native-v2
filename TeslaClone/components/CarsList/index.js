bb0223e7f1c514a1745694307265e35baae2f2ce
isabelchua <isabelchua86@gmail.com>
2021-02-05 21:52:53 +0800

    scroll snap

--- TeslaClone/components/CarsList/index.js
+++ TeslaClone/components/CarsList/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, FlatList } from "react-native";
+import { Text, View, FlatList, Dimensions } from "react-native";
 import CarItem from "../CarItem";
 import styles from "./styles";
 
@@ -7,7 +7,14 @@ import cars from "./cars";
 
 const CarsList = props => (
 	<View style={styles.container}>
-		<FlatList data={cars} renderItem={({ item }) => <CarItem car={item} />} />
+		<FlatList
+			data={cars}
+			renderItem={({ item }) => <CarItem car={item} />}
+			showsVerticalScrollIndicator={false}
+			snapToAlignment={"start"}
+			decelerationRate={"fast"}
+			snapToInterval={Dimensions.get("window").height}
+		/>
 	</View>
 );
 
