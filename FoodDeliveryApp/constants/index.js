043d6dc818de250cef9ecb86d990804c381163bd
isabelchua <isabelchua86@gmail.com>
2021-02-08 22:05:27 +0800

    linked app

---
+++ FoodDeliveryApp/constants/index.js
@@ -0,0 +1,10 @@
+import images from './images';
+import {COLORS, SIZES, FONTS} from './theme';
+import GOOGLE_API_KEY from './maps';
+
+export {icons, images, COLORS, SIZES, FONTS, GOOGLE_API_KEY};
+import images from './images';
+import {COLORS, SIZES, FONTS} from './theme';
+import GOOGLE_API_KEY from './maps';
+
+export {icons, images, COLORS, SIZES, FONTS, GOOGLE_API_KEY};
